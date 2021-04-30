8f2380bc6c987955bdffb06d13b4ebd75f8c7804
h2chen <49930326+h2chen@users.noreply.github.com>
2021-04-30 14:33:29 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -43,10 +43,12 @@ async def setup_learner():
     try:
         #learn = load_learner(path, export_file_name)        
         #learn = keras.models.load_model("app/"+export_file_name)
-        with open(model_config_name, "r") as text_file:
-            json_string = text_file.read()
-        learn = keras.models.model_from_json(json_string)
-        learn.load_weights(model_file_name)
+        #with open(model_config_name, "r") as text_file:
+        #    json_string = text_file.read()
+        #learn = keras.models.model_from_json(json_string)
+        #learn.load_weights(model_file_name)
+        learn = keras.models.load_model(model_path)
+        
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
